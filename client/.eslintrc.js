faf82e92d95efe3767edcf7bb957c893d670c8ea
komangmahendra <komang@hellonimbly.com>
2020-07-02 10:37:37 +0800

    Fix: linter error on test file

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "jest/globals": true,
   },
   extends: [
+    "plugin:jest/recommended",
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
@@ -16,5 +18,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
   },
+  plugins: ["jest"],
 };
